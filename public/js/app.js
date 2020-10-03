910084b5a29ac15c4b0999d0237ef06620265218
mdmac <matthew.mmacdonald@gmail.com>
2020-10-03 15:56:07 +0100

    Added weather from current location button

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
-const weatherForm = document.querySelector('form')
+const mainButton = document.querySelector('.btn-1')
+const secondButton = document.querySelector('.btn-2')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('.message-1')
 const messageTwo = document.querySelector('.message-2')
 
-weatherForm.addEventListener('submit', (e) => {
+mainButton.addEventListener('click', (e) => {
     e.preventDefault()
     const location = search.value
-    
+    console.log(location)
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
@@ -20,4 +21,28 @@ weatherForm.addEventListener('submit', (e) => {
         }
     })
 })
+})
+
+secondButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    if (!navigator.geolocation) {
+        messageOne.textContent = 'Browser does not support geolocation'
+    }
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
+
+        fetch(`/weather?address=${position.coords.longitude},${position.coords.latitude}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+    })
 })
